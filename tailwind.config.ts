83c91b7da05d978e67474791963cfb7973418dea
adanjoserrojas <adanrojas224@gmail.com>
2025-09-27 11:53:40 -0400

    Cleaned Repo and UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,11 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    // Use broader patterns to include more utilities
+    "./app/**/*.{html,js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,html,css}",
+    // This file will act as a "utilities manifest"
+    "./src/utilities.txt",
   ],
   theme: { extend: {} },
   plugins: [],
