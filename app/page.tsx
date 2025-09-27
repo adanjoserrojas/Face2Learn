77d13db25353ea76d32bae4936bc2f737f740b8f
adanjoserrojas <adanrojas224@gmail.com>
2025-09-27 02:28:55 -0400

    Initial Chrome Extension Skeleton

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './globals.css';
+
+
+export default function Home() {
+  return (
+    <main className="h-[800px] w-[600px] flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">D</h1>
+      <p className="mt-4">Awesome</p>
+    </main>
+  );
+}
